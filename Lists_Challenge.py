ad478a706122a213c452798401f7f54c5253dac6
ErinMayo <ErinEValentine@gmail.com>
2024-09-26 16:17:16 +0000

    sql operations and python3

---
+++ Lists_Challenge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Lists_Challenge - testing knowledge of lists"""
+
+def main():
+    ## create list
+    wordbank= ["indentation", "spaces"]
+
+    tlgstudents= ["Aaron", "Andy", "Brian", "Chris", "Cliff", "Daniel", "Erin", "Joseph", "Justin", "Linda", "Michael", "Rupesh", "Samuel", "Zachary"]
+
+    # Part 3
+    wordbank.append(4)
+
+    # Part 4
+
+    print(wordbank)
+
+if __name__ == "__main__":
+    main()
+
+
